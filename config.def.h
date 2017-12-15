1e7a2e6f9c597b0fb6c9d1d48c667dfc23e1e86a
dustin glidden <dglidden@sitscape.com>
2017-12-15 16:12:25 -0500

    bottom bar default

--- config.def.h
+++ config.def.h
@@ -1,9 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char *fonts[] = {
-	"monospace:size=11"
-};
+static const char *fonts[]          = { "monospace:size=11" };
 static const char dmenufont[]       = "monospace:size=11";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#2f3136";
@@ -19,7 +17,7 @@ static const int gappx              = 3;        /* gapsize */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -63,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-b", NULL, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL};
 static const char *termcmd[]  = { "urxvtc", NULL };
 static const char *lockcmd[]  = { "lxlock", NULL };
 static const char *updatexrandr[]  = { "external_on", NULL };
