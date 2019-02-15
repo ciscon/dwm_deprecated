f50f68d6e35ff5dda79b5af54c8c40954e513025
dustin glidden <dglidden@sitscape.com>
2019-02-15 16:18:12 -0500

    update defaults

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
@@ -9,17 +9,18 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "monospace:size=13" };
+static const char dmenufont[]       = "monospace:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_red[]         = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_red },
 };
 static const int gappx              = 3;        /* gapsize */
 
@@ -68,7 +69,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-b", NULL, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *browsercmd[]  = { "google-chrome", NULL };
 static const char *lockcmd[]  = { "lxlock", NULL };
 static const char *xtrlock[]  = { "xtrlock", NULL };
 static const char *screenshotcmd[]  = { "screenshot", NULL };
@@ -82,6 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
     { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = lockcmd } },
+    { MODKEY,                       XK_g,      spawn,          {.v = browsercmd } },
     { MODKEY|ShiftMask,             XK_z,      spawn,          {.v = xtrlock } },
     { MODKEY|ShiftMask,             XK_r,      spawn,          {.v = updatexrandr } },
     { MODKEY,                       XK_s,      spawn,          {.v = screenshotcmd} },
